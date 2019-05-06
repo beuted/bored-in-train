604da83c2c05e4c7432972820f3678c6da81d96a
Benoit Jehanno <b.jehanno@criteo.com>
2019-05-06 12:57:25 +0200

    Use setTimout instead of setIntervals

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -41,7 +41,7 @@ import Map from '@/components/Map.vue';
 export default class Game extends IdleGameVue {
   @Prop() private msg!: string;
 
-  private readonly TickInterval = 1000;
+  private TickInterval = 1000;
   private readonly StarvationFactor = 0.5; // Portion of disapearing goods when missing consummable
   private readonly LackOfStorageFactor = 1.0; // Portion of disapearing goods when missing storage
 
@@ -58,7 +58,12 @@ export default class Game extends IdleGameVue {
   }
 
   public toggleDebug() {
-      this.$store.commit('ToggleDebugMode');
+    this.$store.commit('ToggleDebugMode');
+    if (this.TickInterval == 1000) {
+      this.TickInterval = 50;
+    } else {
+      this.TickInterval = 1000
+    }
   }
 
   public reset() {
@@ -67,107 +72,119 @@ export default class Game extends IdleGameVue {
   }
 
   public mounted() {
+    this.mainLoop();
+    this.discoveryLoop();
+
     console.log(`I'm bored in a train`);
+  }
 
-    setInterval (() => {
-        let store: Store<IState> = this.$store;
+  private mainLoop() {
+    let store: Store<IState> = this.$store;
 
-        // First the creation of ressources
-        for (let jobId in StaticJobInfo) {
-            let staticJob: IStaticJob = StaticJobInfo[jobId as Job]; //TODO: fix typeing weirdlness
+    // First the creation of ressources
+    for (let jobId in StaticJobInfo) {
+      let staticJob: IStaticJob = StaticJobInfo[jobId as Job]; //TODO: fix typeing weirdlness
 
-            // Create ProductionEvent
-            var event: IJobProductionEvent = {
-                job: jobId as Job,
-                produced: {
-                    population: 0,
-                    food: 0,
-                    wood: 0,
-                    stones: 0,
-                    coals: 0,
-                    energy: 0,
-                    knowledge: 0
-                }
-            }
-
-            if (store.state.jobs[jobId as Job].remainingTime > 0) {
-                store.commit('TickInterval', { job: jobId });
-                continue;
-            }
-
-            store.commit('ResetInterval', { job: jobId });
-
-            for (let consummableId in staticJob.produce) {
-                let staticJobProduction: IStaticJobProduction | null = staticJob.produce[consummableId as Consummable];
-                if (staticJobProduction == null)
-                    continue;
-
-                var nbProducers = store.state.jobs[jobId as Job].quantity;
-                event.produced[consummableId as Consummable] += nbProducers * staticJobProduction.quantity;
-            }
-
-            let nbUnfullfiledWorkers = 0; // Nb workers that have not been receiving ressources therefore will be deduced from production
-            for (let consummableId in staticJob.consume) {
-                let staticJobConsumption: IStaticJobProduction | null = staticJob.consume[consummableId as Consummable];
-                if (staticJobConsumption == null)
-                    continue;
-
-                var nbConsummer = store.state.jobs[jobId as Job].quantity;
-                if (store.state.consummable[consummableId as Consummable].quantity >= nbConsummer * staticJobConsumption.quantity) {
-                    event.produced[consummableId as Consummable] -= nbConsummer * staticJobConsumption.quantity;
-                } else {
-                    // Do not produce if needs not fulfilled!
-                    const whatCanBeconsummed = store.state.consummable[consummableId as Consummable].quantity;
-                    let nbUnfullfiledWorkersOnConsummable = Math.floor((nbConsummer - store.state.consummable[consummableId as Consummable].quantity) / staticJobConsumption.quantity);
-                    nbUnfullfiledWorkers = Math.max(nbUnfullfiledWorkers, nbUnfullfiledWorkersOnConsummable);
-
-                    event.produced[consummableId as Consummable] -= whatCanBeconsummed;
-                }
-            }
-
-
-            // Remove part of production based on number of workers with non-fullfiled needs
-            if (nbUnfullfiledWorkers > 0) {
-                this.$toasted.error(`${nbUnfullfiledWorkers} ${jobId} have not seen their needs fullfiled, they will not produce any ressource`);
-                for (let consummableId in staticJob.produce) {
-                    let staticJobProduction: IStaticJobProduction | null = staticJob.produce[consummableId as Consummable];
-                    if (staticJobProduction == null)
-                        continue;
-
-                    event.produced[consummableId as Consummable] -= nbUnfullfiledWorkers * staticJobProduction.quantity;
-                }
-            }
-
-            store.commit('IncrementConsummables', event);
-            EventBus.$emit('job-production', event);
+      // Create ProductionEvent
+      var event: IJobProductionEvent = {
+        job: jobId as Job,
+        produced: {
+          population: 0,
+          food: 0,
+          wood: 0,
+          stones: 0,
+          coals: 0,
+          energy: 0,
+          knowledge: 0
         }
-
-        // After operation checks (storage, ...)
-        for (let consummableId in StaticConsummableInfo) {
-            let staticConsummable: IStaticConsummable = StaticConsummableInfo[consummableId as Consummable]; //TODO: fix typeing weirdlness
-            // See if storage fits
-            if (staticConsummable.storage && staticConsummable.storage.capacity * store.state.map.storage[staticConsummable.storage.name].quantity < store.state.consummable[consummableId as Consummable].quantity) {
-                let quantityToRemove = Math.floor(this.LackOfStorageFactor *
-                    (store.state.consummable[consummableId as Consummable].quantity - store.state.map.storage[staticConsummable.storage.name].quantity * staticConsummable.storage.capacity));
-                store.commit('IncrementConsummable', { name: consummableId, value: -quantityToRemove });
-                // this.$toasted.error(`${quantityToRemove} ${consummableId} were thrown away due to lack of storage`);
-            }
-        }
-
-    }, this.TickInterval);
-
-    setInterval (() => {
-      var nbExplorers = this.$store.state.jobs.explorer.quantity;
-      if (nbExplorers <= 0)
-        return;
-
-      var nbLandFound = this.$store.state.map.mapNbTileFound;
-      // Probabilty in proba of at least 1 explorer out of nbExplorers to find a tile
-      var probability = 1-Math.pow(1-2/nbLandFound, nbExplorers);
-      if (Math.random() <= probability) {
-         this.$toasted.success(`Land Found! (Probability was: ${probability.toPrecision(2)})`);
-         this.$store.commit('DiscoverTile');
       }
+
+      if (store.state.jobs[jobId as Job].remainingTime > 0) {
+        store.commit('TickInterval', { job: jobId });
+        continue;
+      }
+
+      store.commit('ResetInterval', { job: jobId });
+
+      for (let consummableId in staticJob.produce) {
+        let staticJobProduction: IStaticJobProduction | null = staticJob.produce[consummableId as Consummable];
+        if (staticJobProduction == null)
+          continue;
+
+        var nbProducers = store.state.jobs[jobId as Job].quantity;
+        event.produced[consummableId as Consummable] += nbProducers * staticJobProduction.quantity;
+      }
+
+      let nbUnfullfiledWorkers = 0; // Nb workers that have not been receiving ressources therefore will be deduced from production
+      for (let consummableId in staticJob.consume) {
+        let staticJobConsumption: IStaticJobProduction | null = staticJob.consume[consummableId as Consummable];
+        if (staticJobConsumption == null)
+          continue;
+
+        var nbConsummer = store.state.jobs[jobId as Job].quantity;
+        if (store.state.consummable[consummableId as Consummable].quantity >= nbConsummer * staticJobConsumption.quantity) {
+          event.produced[consummableId as Consummable] -= nbConsummer * staticJobConsumption.quantity;
+        } else {
+          // Do not produce if needs not fulfilled!
+          const whatCanBeconsummed = store.state.consummable[consummableId as Consummable].quantity;
+          let nbUnfullfiledWorkersOnConsummable = Math.floor((nbConsummer - store.state.consummable[consummableId as Consummable].quantity) / staticJobConsumption.quantity);
+          nbUnfullfiledWorkers = Math.max(nbUnfullfiledWorkers, nbUnfullfiledWorkersOnConsummable);
+
+          event.produced[consummableId as Consummable] -= whatCanBeconsummed;
+        }
+      }
+
+
+      // Remove part of production based on number of workers with non-fullfiled needs
+      if (nbUnfullfiledWorkers > 0) {
+        this.$toasted.error(`${nbUnfullfiledWorkers} ${jobId} have not seen their needs fullfiled, they will not produce any ressource`);
+        for (let consummableId in staticJob.produce) {
+          let staticJobProduction: IStaticJobProduction | null = staticJob.produce[consummableId as Consummable];
+          if (staticJobProduction == null)
+            continue;
+
+          event.produced[consummableId as Consummable] -= nbUnfullfiledWorkers * staticJobProduction.quantity;
+        }
+      }
+
+      store.commit('IncrementConsummables', event);
+      EventBus.$emit('job-production', event);
+    }
+
+    // After operation checks (storage, ...)
+    for (let consummableId in StaticConsummableInfo) {
+      let staticConsummable: IStaticConsummable = StaticConsummableInfo[consummableId as Consummable]; //TODO: fix typeing weirdlness
+      // See if storage fits
+      if (staticConsummable.storage && staticConsummable.storage.capacity * store.state.map.storage[staticConsummable.storage.name].quantity < store.state.consummable[consummableId as Consummable].quantity) {
+        let quantityToRemove = Math.floor(this.LackOfStorageFactor *
+          (store.state.consummable[consummableId as Consummable].quantity - store.state.map.storage[staticConsummable.storage.name].quantity * staticConsummable.storage.capacity));
+        store.commit('IncrementConsummable', { name: consummableId, value: -quantityToRemove });
+        // this.$toasted.error(`${quantityToRemove} ${consummableId} were thrown away due to lack of storage`);
+      }
+    }
+
+    // Recursive setTimeout for precision
+    setTimeout (() => {
+      this.mainLoop()
+    }, this.TickInterval);
+  }
+
+  private discoveryLoop() {
+    var nbExplorers = this.$store.state.jobs.explorer.quantity;
+    if (nbExplorers <= 0)
+      return;
+
+    var nbLandFound = this.$store.state.map.mapNbTileFound;
+    // Probabilty in proba of at least 1 explorer out of nbExplorers to find a tile
+    var probability = 1-Math.pow(1-2/nbLandFound, nbExplorers);
+    if (Math.random() <= probability) {
+      this.$toasted.success(`Land Found! (Probability was: ${probability.toPrecision(2)})`);
+      this.$store.commit('DiscoverTile');
+    }
+
+    // Recursive setTimeout for precision
+    setTimeout (() => {
+      this.discoveryLoop()
     }, 10 * this.TickInterval);
   }
 }
@@ -176,12 +193,12 @@ export default class Game extends IdleGameVue {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
 .flex-container {
-    height: 100%;
-    padding: 0;
-    margin: 0;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
+  height: 100%;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 }
 
 .inventory-item {

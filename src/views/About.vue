3663d2dd19c05e00a31354bf9f7248dc6c3dcc88
Benoit Jehanno <b.jehanno@criteo.com>
2019-06-01 06:47:57 -0400

    Add about text

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>About</h1>
+    <div class="column">
+      <p>
+        I started this dumb Idle game because I was bored on the train. I drop it right after (like most of my personnal projects ⚰).
+        Started it over and moved it to vuejs because I wanted to learn, eventually I got a nice idea: What if I was trying to make an idle game
+        with a bit of <i>pathos</i> in it ? Anyway I learned a bit doing it and had some fun myself. I hope you'll like it.
+      </p>
+      <p>
+        @dekajoo
+      </p>
+    </div>
   </div>
 </template>
+
+<style lang="less">
+.column {
+  width: 800px;
+  margin: auto;
+  text-align: left;
+}
+</style>

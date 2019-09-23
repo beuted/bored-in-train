557f2899c3be08b529539c15dbdb46930dc7c865
Benoit Jehanno <b.jehanno@criteo.com>
2019-09-23 17:52:22 +0200

    Fix Reset

--- src/store/storeSaver.ts
+++ src/store/storeSaver.ts
@@ -32,6 +32,7 @@ export class StoreSaver {
     console.log('Resetting...');
     window.localStorage.setItem(StoreSaver.storageKey, JSON.stringify({}));
     store.commit('StoreSaverRestore', {});
+    location.reload();
     console.log('Reset.');
   }
 
